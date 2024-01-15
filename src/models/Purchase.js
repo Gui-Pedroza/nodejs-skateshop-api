55b4c6e46bfef6f91fe89edf2e5c61594e2170ec
Gui-Pedroza <gui_pedroza@hotmail.com>
2024-01-15 16:09:28 -0300

    git ignore

---
+++ src/models/Purchase.js
@@ -0,0 +1,14 @@
+import { sql } from '../config/db.js'
+import { v4 as uuidv4 } from 'uuid'
+class Purchase {
+    constructor(id, product_id, client_id, quantity) {
+        this.id = id
+        this.product_id = product_id
+        this.client_id = client_id
+        this.quantity = quantity
+    }
+
+    getProductsByClient(client_id) {
+        
+    }
+}
